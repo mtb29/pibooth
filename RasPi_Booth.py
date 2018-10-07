88e359df81ae71582c590688903ca81364a2f431
mtb29 <mtb29@users.noreply.github.com>
2018-10-06 22:58:13 -0400

    Version 4 released
    Version 4 changes:
    
    (very basic) logging enabled.
    added feature to print a collage twice
    (hopefully) fixed bug in v3 in regards to updating the displayed photo
    montage has been updated to 2x2 with first image being a frame/logo image for the collage
    added placeholder frame image
    print photos button is now optional

--- RasPi_Booth.py
+++ RasPi_Booth.py
@@ -1,8 +1,9 @@
 #! /usr/bin/python3
 # Raspberry Pi photobooth with printer (Canon SELPHY CP1200) and touchscreen (Using Kivy)
 # Captures 3 images and creates them into a collage, and (optionally) prints 2 copies per page
-# Version: 0.3
+# Version: 0.4
 # By: Matthew Brady, Andrea Lynn
+# Enclosure by: Nick Natale
 
 import picamera
 import cups
@@ -25,7 +26,7 @@ from kivy.graphics import Color, Rectangle
 
 # Variables
 saveDir = "/home/pi/photobooth/save" # Where the pictures that are taken are saved
-logDir = "/home/pi/photobooth/logs" # Where the log is saved
+logDir = "/home/pi/photobooth/logs/RasPi_Booth.log" # Where the log is saved
 collageName = "collage.jpg" # Name of the collage photo
 collageNameSmall = "collage_small.jpg" # Name of the small collage photo for displaying on the screen
 collageCreated = False # Script processing
@@ -33,62 +34,87 @@ collageTime = 0 # Script processing
 collageTimeout = 25 # How long the latest collage will display on the screen
 logoName = "logo.jpg" # Name of the logo photo
 enablePrinter = True # Set to False to disable the printer functionality + display
+enablePhotoPrint = False # Set to True to enable printing each individual picture
+enableTwoCopies = True # Set to True to enable printing two copies of the collage
 printerName = "CP1200" # Set to the name you configured the printer in on the CUPS server
 printerBusy = False # Script processing
 printerTime = 0 # Script processing
 printerTimeout = 60 # Script processing
 printingName = "printing.jpg" # Name of the printing/printer busy photo
+frameName = "frame.jpg" # Name of the frame photo
 cupsUser = "pi" # If you are using a username other than the default, change it here
 photoExt = ".jpg" # Set to your desired photo extension. If not using jpg, you may have to install different libraries
 photoName = time.strftime("%Y_%m_%d_%H:%M:%S") # File name format for the backup images
 
+# Logging configuration
+logging.basicConfig(filename=logDir,level=logging.DEBUG)
+logging.basicConfig(format='[%(asctime)s] %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
+
 # Function to capture and save photographs.
 def takePhotos(obj):
+	logging.info('Function takePhotos was called.')
+	
     if printerBusy == False:
 		camera = picamera.PiCamera()
 		camera.resolution = (1280, 720)
 		camera.start_preview()
-		sleep(3)
+		sleep(5)
 		camera.capture('1.jpg')
 		photoName = time.strftime("%Y_%m_%d_%H:%M:%S")
 		copyfile('1.jpg', saveDir + "/" + photoName + photoExt)
 		camera.start_preview()
-		sleep(3)
+		sleep(5)
 		camera.capture('2.jpg')
 		photoName = time.strftime("%Y_%m_%d_%H:%M:%S")
 		copyfile('2.jpg', saveDir + "/" + photoName + photoExt)
 		camera.start_preview()
-		sleep(3)
+		sleep(5)
 		camera.capture('3.jpg')
 		photoName = time.strftime("%Y_%m_%d_%H:%M:%S")
 		copyfile('3.jpg', saveDir + "/" + photoName + photoExt)
 		camera.close()
 		createCollage(obj)
-        
+		logging.info('Function takePhotos completed.')
+ 
+# Function to create the collage 
 def createCollage(obj):
-    subprocess.call(["montage", '1.jpg', '1.jpg', '2.jpg', '2.jpg', '3.jpg', '3.jpg', "-tile", "2x3", "-geometry", "+2+2", collageName])
-    subprocess.call(["montage", '1.jpg', '1.jpg', '2.jpg', '2.jpg', '3.jpg', '3.jpg', "-tile", "2x3", "-geometry", "640x640+2+2", collageNameSmall])
+	logging.info('Function createCollage was called.')
+    #subprocess.call(["montage", '1.jpg', '1.jpg', '2.jpg', '2.jpg', '3.jpg', '3.jpg', "-tile", "2x3", "-geometry", "+2+2", collageName])
+    #subprocess.call(["montage", '1.jpg', '1.jpg', '2.jpg', '2.jpg', '3.jpg', '3.jpg', "-tile", "2x3", "-geometry", "640x640+2+2", collageNameSmall])
+	subprocess.call(["montage", frameName, '1.jpg', '2.jpg', '3.jpg', "-tile", "2x2", "-geometry", "+2+2", "-background", "black", collageName])
+    subprocess.call(["montage", frameName, '1.jpg', '2.jpg', '3.jpg', "-tile", "2x2", "-geometry", "640x640+2+2", "-background", "black", collageNameSmall])
     global collageCreated
     collageCreated = True
     global collageTime
     collageTime = 0
     photoName = time.strftime("%Y_%m_%d_%H:%M:%S")
     copyfile(collageName, saveDir + "/" + photoName + "_collage" + photoExt)
+	logging.info('Function createCollage completed.')
 
 # Function to send it to the printer for printing (Using CUPS)          
 def printCollage(obj):
+	logging.info('Function printCollage was called.')
     if printerBusy == False:
 		conn = cups.Connection()
 		cups.setUser(cupsUser)
-		conn.printFile(printerName, collageName, "RasPi_Booth", {'fit-to-page':'True'})
 		global printerTime
 		global printerTimeout
 		global printerBusy
 		printerTime = 0
-		printerTimeout = 60
 		printerBusy = True
+		
+		if enableTwoCopies == False:
+			conn.printFile(printerName, collageName, "RasPi_Booth", {'fit-to-page':'True'})
+			printerTimeout = 60
+		else:
+			conn.printFile(printerName, collageName, "RasPi_Booth", {'fit-to-page':'True'})
+			conn.printFile(printerName, collageName, "RasPi_Booth", {'fit-to-page':'True'})
+			printerTimeout = 120
+		logging.info('Function printCollage completed.')
 
+# Function to print each individual photo.
 def printPhotos(obj):
+	logging.info('Function printPhotos was called.')
     if printerBusy == False:
 		conn = cups.Connection()
 		cups.setUser(cupsUser)
@@ -101,8 +127,9 @@ def printPhotos(obj):
 		printerTime = 0
 		printerTimeout = 180
 		printerBusy = True
+		logging.info('Function printPhotos completed.')
 
-
+# Main code of the application
 class RasPi_Booth(App):
     photo = kivyImage(source=logoName) # Display logo on first launch
 
@@ -127,18 +154,20 @@ class RasPi_Booth(App):
         
         if enablePrinter == True:
             photobox.add_widget(printCollageButton)
-            photobox.add_widget(printPhotosButton)
+			
+		if enablePhotoPrint == True:
+			photobox.add_widget(printPhotosButton)
 
         return photobox
         
         
-    # Function to update the display to the lastest collage photo/logo on timeout/printer busy
+    # Function to update the display to the latest collage photo/logo on timeout/printer busy
     def photoUpdate(self, instance):
-        global collageTime
-        global collageCreated
+    global collageTime
+    global collageCreated
 	global printerTime
 	global printerBusy
-        collageTime += 3
+    collageTime += 3
 	printerTime += 3
         
 		if printerBusy == False:
@@ -149,16 +178,15 @@ class RasPi_Booth(App):
 					if collageTime >= collageTimeout:
 						self.photo.source = logoName
 						collageCreated = False
-			else:
+			elif collageCreated == False:
 				if self.photo.source != logoName:
-					self.photo.source = logoName
-		else:
+					self.photo.source = logoName	
+		elif printerBusy == True:
 			if self.photo.source != printingName:
 				self.photo.source = printingName
 			if printerTime >= printerTimeout:
 				self.photo.source = logoName
 				printerBusy = False
-
 
 if __name__ == '__main__':
     RasPi_Booth().run()
