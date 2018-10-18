6fdec7bba8a9d303dcde527dee9e0cbe2676c2ec
mtb29 <mtb29@users.noreply.github.com>
2018-10-18 13:10:43 -0400

    update
    Several fixes

--- RasPi_Booth.py
+++ RasPi_Booth.py
@@ -1,7 +1,7 @@
 #! /usr/bin/python3
 # Raspberry Pi photobooth with printer (Canon SELPHY CP1200) and touchscreen (Using Kivy)
 # Captures 3 images and creates them into a collage, and (optionally) prints 2 copies per page
-# Version: 0.4b
+# Version: 0.4c
 # Programmar: Matthew Brady
 # Designer/Artist: Andrea Brady
 # Woodworker: Nick Natale
@@ -10,6 +10,7 @@ import picamera
 import cups
 import time
 from time import sleep
+import os
 import sys
 from shutil import copyfile
 import logging
@@ -28,24 +29,29 @@ from kivy.graphics import Color, Rectangle
 # Variables
 saveDir = "/home/pi/photobooth/save" # Where the pictures that are taken are saved
 logDir = "/home/pi/photobooth/logs/RasPi_Booth.log" # Where the log is saved
+photoDir = "/home/pi/photobooth/photos/" # Where the magic happens with the photos
+photoOne = photoDir + "1.jpg" # First taken photo
+photoTwo = photoDir + "2.jpg" # Second taken photo
+photoThree = photoDir + "3.jpg" # Third taken photo
 collageName = "collage.jpg" # Name of the collage photo
 collageNameSmall = "collage_small.jpg" # Name of the small collage photo for displaying on the screen
 collageCreated = False # Script processing
 collageTime = 0 # Script processing
-collageTimeout = 25 # How long the latest collage will display on the screen
-logoName = "logo.jpg" # Name of the logo photo
+collageTimeout = 45 # How long the latest collage will display on the screen
+logoName = photoDir + "logo.jpg" # Name of the logo photo
 enablePrinter = True # Set to False to disable the printer functionality + display
 enablePhotoPrint = False # Set to True to enable printing each individual picture
-enableTwoCopies = False # Set to True to enable printing two copies of the collage
-printerName = "CP1200" # Set to the name you configured the printer in on the CUPS server
+enableTwoCopies = True # Set to True to enable printing two copies of the collage
+printerName = "CP1300" # Set to the name you configured the printer in on the CUPS server
 printerBusy = False # Script processing
 printerTime = 0 # Script processing
 printerTimeout = 60 # Script processing
-printingName = "printing.jpg" # Name of the printing/printer busy photo
-frameName = "frame.jpg" # Name of the frame photo
+printingName = photoDir + "printing.jpg" # Name of the printing/printer busy photo
+frameDir = "/home/pi/photobooth/frames/" # Where the frames are stored
+frameName = frameDir + "101318.jpg" # Name of the frame photo
 cupsUser = "pi" # If you are using a username other than the default, change it here
 photoExt = ".jpg" # Set to your desired photo extension. If not using jpg, you may have to install different libraries
-photoName = time.strftime("%Y_%m_%d_%H:%M:%S") # File name format for the backup images
+photoName = time.strftime("%Y_%m_%d_%H_%M_%S") # File name format for the backup images
 
 # Logging configuration
 logging.basicConfig(filename=logDir,level=logging.DEBUG)
@@ -55,24 +61,24 @@ logging.basicConfig(format='[%(asctime)s] %(message)s', datefmt='%m/%d/%Y %I:%M:
 def takePhotos(obj):
     logging.info('Function takePhotos was called.')
 	
-    if printerBusy == False:
+    if printerBusy == False and collageCreated == False:
         camera = picamera.PiCamera()
         camera.resolution = (1280, 720)
         camera.start_preview()
-        sleep(5)
-        camera.capture('1.jpg')
-        photoName = time.strftime("%Y_%m_%d_%H:%M:%S")
-        copyfile('1.jpg', saveDir + "/" + photoName + photoExt)
+        sleep(10)
+        camera.capture(photoOne)
+        photoName = time.strftime("%Y_%m_%d_%H_%M_%S")
+        copyfile(photoOne, saveDir + "/" + photoName + photoExt)
         camera.start_preview()
         sleep(5)
-        camera.capture('2.jpg')
-        photoName = time.strftime("%Y_%m_%d_%H:%M:%S")
-        copyfile('2.jpg', saveDir + "/" + photoName + photoExt)
+        camera.capture(photoTwo)
+        photoName = time.strftime("%Y_%m_%d_%H_%M_%S")
+        copyfile(photoTwo, saveDir + "/" + photoName + photoExt)
         camera.start_preview()
         sleep(5)
-        camera.capture('3.jpg')
-        photoName = time.strftime("%Y_%m_%d_%H:%M:%S")
-        copyfile('3.jpg', saveDir + "/" + photoName + photoExt)
+        camera.capture(photoThree)
+        photoName = time.strftime("%Y_%m_%d_%H_%M_%S")
+        copyfile(photoThree, saveDir + "/" + photoName + photoExt)
         camera.close()
         createCollage(obj)
         logging.info('Function takePhotos completed.')
@@ -80,16 +86,16 @@ def takePhotos(obj):
 # Function to create the collage 
 def createCollage(obj):
     logging.info('Function createCollage was called.')
-    #subprocess.call(["montage", '1.jpg', '1.jpg', '2.jpg', '2.jpg', '3.jpg', '3.jpg', "-tile", "2x3", "-geometry", "+2+2", collageName])
-    #subprocess.call(["montage", '1.jpg', '1.jpg', '2.jpg', '2.jpg', '3.jpg', '3.jpg', "-tile", "2x3", "-geometry", "640x640+2+2", collageNameSmall])
-    subprocess.call(["montage", frameName, '1.jpg', '2.jpg', '3.jpg', "-tile", "2x2", "-geometry", "+2+2", "-background", "white", collageName])
-    subprocess.call(["montage", frameName, '1.jpg', '2.jpg', '3.jpg', "-tile", "2x2", "-geometry", "720x720+2+2", "-background", "white", collageNameSmall])
+    subprocess.call(["montage", frameName, photoOne, photoTwo, photoThree, "-tile", "2x2", "-geometry", "+2+2", "-background", "white", photoDir + collageName])
     global collageCreated
     collageCreated = True
     global collageTime
     collageTime = 0
-    photoName = time.strftime("%Y_%m_%d_%H:%M:%S")
-    copyfile(collageName, saveDir + "/" + photoName + "_collage" + photoExt)
+    photoName = time.strftime("%Y_%m_%d_%H_%M_%S")
+    global collageNameSmall
+    collageNameSmall = photoDir + photoName + "_collage_small" + photoExt
+    subprocess.call(["montage", frameName, photoOne, photoTwo, photoThree, "-tile", "2x2", "-geometry", "720x720+2+2", "-background", "white", collageNameSmall])
+    copyfile(photoDir + collageName, saveDir + "/" + photoName + "_collage" + photoExt)
     logging.info('Function createCollage completed.')
 
 # Function to send it to the printer for printing (Using CUPS)          
@@ -106,10 +112,10 @@ def printCollage(obj):
         printerBusy = True
 		
         if enableTwoCopies == False:
-            conn.printFile(printerName, collageName, "RasPi_Booth", {'fit-to-page':'True'})
+            conn.printFile(printerName, photoDir + collageName, "RasPi_Booth", {'fit-to-page':'True'})
             printerTimeout = 60
         else:
-            conn.printFile(printerName, collageName, "RasPi_Booth", {'fit-to-page':'True', 'copies':'2'})
+            conn.printFile(printerName, photoDir + collageName, "RasPi_Booth", {'fit-to-page':'True', 'copies':'2'})
             printerTimeout = 120
             
         logging.info('Function printCollage completed.')
@@ -122,9 +128,9 @@ def printPhotos(obj):
     if printerBusy == False:
         conn = cups.Connection()
         cups.setUser(cupsUser)
-        conn.printFile(printerName, "1.jpg", "RasPi_Booth", {'fit-to-page':'True'})
-        conn.printFile(printerName, "2.jpg", "RasPi_Booth", {'fit-to-page':'True'})
-        conn.printFile(printerName, "3.jpg", "RasPi_Booth", {'fit-to-page':'True'})
+        conn.printFile(printerName, photoOne, "RasPi_Booth", {'fit-to-page':'True'})
+        conn.printFile(printerName, photoTwo, "RasPi_Booth", {'fit-to-page':'True'})
+        conn.printFile(printerName, photoThree, "RasPi_Booth", {'fit-to-page':'True'})
         global printerTime
         global printerTimeout
         printerTime = 0
@@ -181,6 +187,11 @@ class RasPi_Booth(App):
                     if collageTime >= collageTimeout:
                         self.photo.source = logoName
                         collageCreated = False
+                        os.remove(photoOne)
+                        os.remove(photoTwo)
+                        os.remove(photoThree)
+                        os.remove(photoDir + collageName)
+                        os.remove(collageNameSmall)
             elif collageCreated == False:
                 if self.photo.source != logoName:
                     self.photo.source = logoName	
